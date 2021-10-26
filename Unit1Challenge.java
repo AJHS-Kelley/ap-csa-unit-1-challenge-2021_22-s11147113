722cabbf56d261afba0395a3d8eea8486562b63c
Josiah W <70596172+s11147113@users.noreply.github.com>
2021-10-26 08:42:35 -0400

    Update Unit1Challenge.java

--- Unit1Challenge.java
+++ Unit1Challenge.java
@@ -1,5 +1,9 @@
 // <Student Name>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <Date>, <Time>, v0.0
 // peer bugging b mauricio salmoran
 public class ? {
-    
+int numAttacks;
+double dmgPerAttack;    
+String WeaponName
+
+
 }
